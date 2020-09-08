697c2a82e36a214c6c47d5025d60c3e6221ab49c
Gorg0n <49187086+Gorg0n@users.noreply.github.com>
2020-09-08 14:55:34 +0200

    finita RANS

---
+++ cleanRANS.py
@@ -0,0 +1,93 @@
+import pandas as pd
+
+def calcSW(df,dudx,dudy,dudz,dvdx,dvdy,dvdz,dwdx,dwdy,dwdz):
+    df['S']=0.0
+    df['W']=0.0
+
+    for ind in df.index:
+        row = df.loc[ind]
+        a = row[dudx]
+        b = row[dudy]
+        c = row[dudz]
+
+        d = row[dvdx]
+        e = row[dvdy]
+        f = row[dvdz]
+
+        g = row[dwdx]
+        h = row[dwdy]
+        i = row[dwdz]
+
+        A = a
+        B = 0.5 * (b + d)
+        C = 0.5 * (c + g)
+
+        D = 0.5 * (d + b)
+        E = e
+        F = 0.5 * (f + h)
+
+        G = 0.5 * (g + c)
+        H = 0.5 * (h + f)
+        I = i
+
+        S = A**2 + B **2 + C**2 + D**2 +E**2 +F**2 +G**2 +H**2 + I**2
+        S = S ** 0.5
+
+        df.at[ind,'S']=S
+
+        B = 0.5 * (b - d)
+        C = 0.5 * (c - g)
+
+        D = 0.5 * (d - b)
+        F = 0.5 * (f - h)
+
+        G = 0.5 * (g - c)
+        H = 0.5 * (h - f)
+
+        W = B **2 + C**2 + D**2 +F**2 +G**2 +H**2
+        W = W ** 0.5
+
+        df.at[ind,'W']=W
+    return
+
+
+
+df = pd.read_csv('inputFiles/RANS.csv', low_memory=False,skiprows=5)
+print(df.columns)
+df.columns= \
+    ['x','y','z','CH4MF','dCH4MF','nut','dnut','p','dp','T','epsilon','depsilon','k','dk','velocity',
+     'dudx','dudy','dudz','dvdx','dvdy','dvdz','dwdx','dwdy','dwdz','WSSx', 'WSSy','WSSz','a','b','c']
+
+print(df.describe())
+print(df.shape)
+#df=df.sample(10000,random_state=42)
+print(df.describe())
+print(df.shape)
+#df.to_hdf('outputFiles/RANSsampled.h5', key='df', mode='w')
+print('dropping stuff')
+df.drop(columns=['WSSy','WSSz','a','b','c'],inplace=True)
+print('calculate S and W')
+calcSW(df,'dudx','dudy','dudz','dvdx','dvdy','dvdz','dwdx','dwdy','dwdz')
+print('dropping stuff again')
+df.drop(columns=['dudx','dudy','dudz','dvdx','dvdy','dvdz','dwdx','dwdy','dwdz'],inplace=True)
+print(df.columns)
+
+n = 0
+df['BC']=0
+for ind, el in enumerate(df['WSSx']):
+    if el == ' null':
+        n += 1
+    else:
+        df.at[ind,'BC']=1
+m = len(df['WSSx']) - n
+
+df2 = pd.DataFrame(df.loc[df['WSSx'] != ' null'])
+df2.drop(columns=['WSSx'],inplace=True)
+df2.to_csv('outputFiles/boundaryRANS.csv')
+df2.to_hdf('outputFiles/boundaryRANS.h5', key='df', mode='w')
+print('written boundary file')
+df.drop(columns=['WSSx'],inplace=True)
+df.to_hdf('outputFiles/RANSclean.h5', key='df', mode='w')
+df.to_csv('outputFiles/RANSclean.csv')
+print('written csv file')
+print(m,n,len(df2))
